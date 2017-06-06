488feda8ad39b68901cce0ecc411148ba9944727
MartinBG <MartinBG@abv.bg>
2017-06-07 01:58:44 +0300

    10 - lab

---
+++ 10. Heaps and Priority Queue/Heaps-Priority-Queues/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count => this.heap.Count;
+
+    public void Insert(T item)
+    {
+        this.heap.Add(item);
+        this.HeapifyUp(this.Count - 1);
+    }
+
+    private void HeapifyUp(int index)
+    {
+        while (index > 0 && IsLess(Parent(index), index))
+        {
+            this.Swap(index, Parent(index));
+            index = Parent(index);
+        }
+    }
+
+    private void Swap(int a, int b)
+    {
+        T temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+
+    private bool IsLess(int parent, int index)
+    {
+        return (this.heap[parent].CompareTo(this.heap[index]) < 0);
+    }
+
+    private int Parent(int index)
+    {
+        return (index - 1) / 2;
+    }
+
+    public T Peek()
+    {
+        if (this.Count <= 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        if (this.Count <= 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = this.heap[0];
+
+        this.Swap(0, this.Count - 1);
+        this.heap.RemoveAt(this.Count - 1);
+        this.HeapifyDown(0);
+
+        return item;
+    }
+
+    private void HeapifyDown(int index)
+    {
+        while (index < this.Count / 2)
+        {
+            int child = Left(index);
+
+            if (HasChild(child + 1) && IsLess(child, child + 1))
+            {
+                child++;
+            }
+
+            if (IsLess(child, index))
+            {
+                break;
+            }
+
+            this.Swap(index, child);
+            index = child;
+        }
+    }
+
+    private bool HasChild(int index)
+    {
+        return index + 1 < this.Count;
+    }
+
+    private int Left(int index)
+    {
+        return 2 * index + 1;
+    }
+}
---
+++ 10. Heaps and Priority Queue/Heaps-Priority-Queues/BinaryHeap/Heap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = n / 2; i >= 0; i--)
+        {
+            HeapifyDown(arr, i, n);
+        }
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);
+            HeapifyDown(arr, 0, i);
+        }
+    }
+
+    private static void HeapifyDown(T[] arr, int index, int border)
+    {
+        
+        while (index < border / 2)
+        {
+            int child = 2 * index + 1;
+
+            if (child + 1 < border && IsLess(arr, child, child + 1))
+            {
+                child++;
+            }
+
+            if (IsLess(arr, child, index))
+            {
+                break;
+            }
+
+            Swap(arr, index, child);
+            index = child;
+        }
+    }
+
+    private static bool IsLess(T[] arr, int a, int b)
+    {
+        return (arr[a].CompareTo(arr[b]) < 0);
+    }
+
+    private static void Swap(T[] arr, int a, int b)
+    {
+        T temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
