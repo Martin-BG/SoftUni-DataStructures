2ddf7f099cad389b73aea01fee85dc56a37b17b3
MartinBG <MartinBG@abv.bg>
2017-06-07 01:57:22 +0300

    08 - lab

---
+++ 07. Basic Tree Data Structures - Tree, Binary Tree - Exercises/Exercise/Trees/Tree.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Tree<T>
+{
+    public T Value { get; set; }
+    public Tree<T> Parent { get; set; }
+    public List<Tree<T>> Children { get; private set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+            child.Parent = this;
+        }
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + this.Value);
+
+        foreach (var child in this.Children)
+        {
+            child.Print(indent + 2);
+        }
+    }
+}
+
+public class Program
+{
+    static Dictionary<int, Tree<int>> nodeByValue = new Dictionary<int, Tree<int>>();
+
+    static void Main(string[] args)
+    {
+        ReadTree();
+        //PrintRootNodeValue();
+        //GetRootNode().Print();
+        //PrintLeafNodes();
+        //PrintMiddleNodes();
+        //PrintDeepestNode();
+        //PrintLongestPath();
+        //PrintPathsOfSum();
+        PrintSubtreesOfSum();
+    }
+
+    private static void PrintSubtreesOfSum()
+    {
+        foreach (var root in GetSubtreeWithSum(GetRootNode()))
+        {
+            PrintPreOrder(root);
+            Console.WriteLine();
+        }
+    }
+
+    private static void PrintPreOrder(Tree<int> root)
+    {
+        Console.Write(root.Value + " ");
+        foreach (var child in root.Children)
+        {
+            PrintPreOrder(child);
+        }
+    }
+
+    private static List<Tree<int>> GetSubtreeWithSum(Tree<int> root)
+    {
+        int sum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Subtrees of sum {sum}:");
+
+        var roots = new List<Tree<int>>();
+
+        DFS(root, sum, 0, roots);
+
+        return roots;
+    }
+
+    private static int DFS(Tree<int> node, int target, int current, List<Tree<int>> roots)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        current = node.Value;
+
+        foreach (var child in node.Children)
+        {
+            current += DFS(child, target, current, roots);
+        }
+
+        if (current == target)
+        {
+            roots.Add(node);
+        }
+        return current;
+    }
+
+    private static void PrintPathsOfSum()
+    {
+        int sum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Paths of sum {sum}:");
+
+        List<List<Tree<int>>> paths = GetPathsToAllNodes();
+
+        Stack<List<int>> pathsOfRightSum = new Stack<List<int>>();
+        foreach (var path in paths)
+        {
+            List<int> route = new List<int>();
+            int pathSum = 0;
+            foreach (var node in path)
+            {
+                route.Add(node.Value);
+                pathSum += node.Value;
+            }
+
+            if (pathSum == sum)
+            {
+                pathsOfRightSum.Push(route);
+            }
+        }
+
+        while (pathsOfRightSum.Count > 0)
+        {
+            List<int> route = pathsOfRightSum.Pop();
+            Console.WriteLine(string.Join(" ", route));
+        }
+    }
+
+    private static List<List<Tree<int>>> GetPathsToAllNodes()
+    {
+        List<List<Tree<int>>> paths = new List<List<Tree<int>>>();
+        Stack<List<Tree<int>>> pathsStack = new Stack<List<Tree<int>>>();
+
+        pathsStack.Push(new List<Tree<int>> { GetRootNode() });
+
+        while (pathsStack.Count > 0)
+        {
+            List<Tree<int>> currentList = pathsStack.Pop();
+
+            if (currentList.Last().Children.Count == 0)
+            {
+                paths.Add(currentList);
+            }
+            else
+            {
+                foreach (var child in currentList.Last().Children)
+                {
+                    pathsStack.Push(new List<Tree<int>>(currentList) { child });
+                }
+            }
+        }
+
+        return paths;
+    }
+
+
+    private static void PrintLongestPath()
+    {
+        Stack<int> path = new Stack<int>();
+        Tree<int> currentNode = GetDeepestNode();
+
+        while (currentNode != null)
+        {
+            path.Push(currentNode.Value);
+            currentNode = currentNode.Parent;
+        }
+
+        Console.WriteLine("Longest path: " + string.Join(" ", path.ToArray()));
+    }
+
+    private static Tree<int> GetDeepestNode()
+    {
+        Stack<Tree<int>> stack = new Stack<Tree<int>>();
+        stack.Push(GetRootNode());
+
+        Tree<int> deepestNode = null;
+
+        while (stack.Count > 0)
+        {
+            bool leftChildTaken = false;
+            Tree<int> current = stack.Pop();
+
+            if (current != null && current.Children.Count > 0)
+            {
+                deepestNode = current.Children.ElementAt(0);
+
+                foreach (var child in current.Children)
+                {
+                    stack.Push(child);
+                }
+            }
+        }
+
+        return deepestNode;
+    }
+
+    private static void PrintDeepestNode()
+    {
+        Tree<int> deepestNode = GetDeepestNode();
+
+        if (deepestNode != null)
+        {
+            Console.WriteLine("Deepest node: " + deepestNode.Value);
+        }
+    }
+
+    private static void PrintMiddleNodes()
+    {
+        var nodes = GetMiddleNodes()
+            .Select(x => x.Value)
+            .OrderBy(x => x)
+            .ToArray();
+
+        Console.WriteLine("Middle nodes: " + string.Join(" ", nodes));
+    }
+
+    private static Tree<int>[] GetMiddleNodes()
+    {
+        return nodeByValue.Values
+            .Where(x => x.Parent != null && x.Children.Count != 0)
+            .ToArray();
+    }
+
+    private static void PrintLeafNodes()
+    {
+        var leaves = GetLeafNodes().Select(x => x.Value).ToArray();
+        Array.Sort(leaves);
+        Console.WriteLine("Leaf nodes: " + string.Join(" ", leaves));
+    }
+
+    private static Tree<int>[] GetLeafNodes()
+    {
+        return nodeByValue.Values
+            .Where(x => x.Children.Count == 0)
+            .ToArray();
+    }
+
+    private static void PrintRootNodeValue()
+    {
+        Console.WriteLine("Root node: " + GetRootNode().Value);
+    }
+
+    static Tree<int> GetRootNode()
+    {
+        return nodeByValue.Values.FirstOrDefault(x => x.Parent == null);
+    }
+
+    static void ReadTree()
+    {
+        int nodes = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < nodes - 1; i++)
+        {
+            var edge = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            AddEdge(edge[0], edge[1]);
+        }
+    }
+
+    public static void AddEdge(int parent, int child)
+    {
+        Tree<int> parentNode = GetTreeNodeByValue(parent);
+        Tree<int> childNode = GetTreeNodeByValue(child);
+
+        parentNode.Children.Add(childNode);
+        childNode.Parent = parentNode;
+    }
+
+    static Tree<int> GetTreeNodeByValue(int value)
+    {
+        if (!nodeByValue.ContainsKey(value))
+        {
+            nodeByValue[value] = new Tree<int>(value);
+        }
+
+        return nodeByValue[value];
+    }
+}
+
---
+++ 08. Binary Search Trees - BFS and DFS/Trees/Trees/BinarySearchTree.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class BinarySearchTree<T> where T : IComparable<T>
+{
+    private Node Root;
+
+    public BinarySearchTree()
+    {
+        
+    }
+
+    private BinarySearchTree(Node root)
+    {
+        this.Copy(root);
+    }
+
+    private void Copy(Node node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.Insert(node.Value);
+        this.Copy(node.Left);
+        this.Copy(node.Right);
+    }
+
+    private class Node
+    {
+        public T Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public void Insert(T value)
+    {
+        if (this.Root == null)
+        {
+            this.Root = new Node(value);
+            return;
+        }
+
+        Node current = this.Root;
+        Node parent = null;
+
+        while (current != null)
+        {
+            parent = current;
+            if (value.CompareTo(current.Value) < 0)
+            {
+                current = current.Left;
+            }
+            else if (value.CompareTo(current.Value) > 0)
+            {
+                current = current.Right;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (parent != null)
+        {
+            if (parent.Value.CompareTo(value) < 0)
+            {
+                parent.Right = new Node(value);
+            }
+            else
+            {
+                parent.Left = new Node(value);
+            }
+        }
+    }
+
+    public bool Contains(T value)
+    {
+        Node current = this.Root;
+
+        while (current != null)
+        {
+            if (value.CompareTo(current.Value) < 0)
+            {
+                current = current.Left;
+            }
+            else if (value.CompareTo(current.Value) > 0)
+            {
+                current = current.Right;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return current != null;
+    }
+
+    public void DeleteMin()
+    {
+        if (this.Root == null)
+        {
+            return;
+        }
+
+        Node min = this.Root;
+        Node parent = null;
+
+        while (min.Left != null)
+        {
+            parent = min;
+            min = min.Left;
+        }
+
+        if (parent == null)
+        {
+            this.Root = this.Root.Right;
+        }
+        else if (parent.Left == null)
+        {
+            this.Root = null;
+        }
+        else
+        {
+            parent.Left = null;
+        }
+    }
+
+    public BinarySearchTree<T> Search(T item)
+    {
+        Node current = this.Root;
+
+        while (current != null)
+        {
+            if (item.CompareTo(current.Value) < 0)
+            {
+                current = current.Left;
+            }
+            else if (item.CompareTo(current.Value) > 0)
+            {
+                current = current.Right;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return new BinarySearchTree<T>(current);
+    }
+
+    public IEnumerable<T> Range(T startRange, T endRange)
+    {
+        Queue<T> range = new Queue<T>();
+
+        this.Range(startRange, endRange, range, this.Root);
+
+        return range;
+    }
+
+    private void Range(T startRange, T endRange, Queue<T> range, Node node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        int compareLow = startRange.CompareTo(node.Value);
+        int compareHigh = endRange.CompareTo(node.Value);
+
+        if (compareLow < 0)
+        {
+            this.Range(startRange, endRange, range, node.Left);
+        }
+
+        if (compareLow <= 0 && compareHigh >= 0)
+        {
+            range.Enqueue(node.Value);
+        }
+
+        if (compareHigh > 0)
+        {
+            this.Range(startRange, endRange, range, node.Right);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this.Root, action);
+    }
+
+    private void EachInOrder(Node node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        EachInOrder(node.Left, action);
+        action(node.Value);
+        EachInOrder(node.Right, action);
+    }
+}
+
+public class Launcher
+{
+    public static void Main(string[] args)
+    {
+    }
+}
