71c7a70554778aaf4a284e595ff40bc5f78179ac
MartinBG <MartinBG@abv.bg>
2017-06-30 03:27:52 +0300

    25. Exam Preparation - Scoreboard solution

---
+++ 25. Exam Preparation/Scoreboard/Scoreboard/Scoreboard.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class Scoreboard : IScoreboard
+{
+    private readonly Dictionary<string, string> _usersPaswords;
+
+    private readonly Dictionary<string, string> _gamesPaswords;
+
+    private readonly OrderedDictionary<string, OrderedBag<ScoreboardEntry>> _games;
+
+    private readonly int _maxEntriesToKeep;
+
+    public Scoreboard(int maxEntriesToKeep = 10)
+    {
+        _maxEntriesToKeep = maxEntriesToKeep;
+        _usersPaswords = new Dictionary<string, string>();
+        _gamesPaswords = new Dictionary<string, string>();
+        _games = new OrderedDictionary<string, OrderedBag<ScoreboardEntry>>(string.CompareOrdinal);
+    }
+
+    public bool RegisterUser(string username, string password)
+    {
+        if (_usersPaswords.ContainsKey(username))
+        {
+            return false;
+        }
+
+        _usersPaswords.Add(username, password);
+
+        return true;
+    }
+
+    public bool RegisterGame(string game, string password)
+    {
+        if (_gamesPaswords.ContainsKey(game))
+        {
+            return false;
+        }
+
+        _gamesPaswords.Add(game, password);
+
+        _games.Add(game, new OrderedBag<ScoreboardEntry>());
+
+        return true;
+    }
+
+    public bool AddScore(string username, string userPassword, 
+        string game, string gamePassword, int score)
+    {
+        if (!_usersPaswords.ContainsKey(username)
+            || !_usersPaswords[username].Equals(userPassword)
+            || !_gamesPaswords.ContainsKey(game)
+            || !_gamesPaswords[game].Equals(gamePassword))
+        {
+            return false;
+        }
+
+        _games[game].Add(new ScoreboardEntry(username, score));
+
+        return true;
+    }
+
+    public IEnumerable<ScoreboardEntry> ShowScoreboard(string game)
+    {
+        if (!_gamesPaswords.ContainsKey(game))
+        {
+            return null;
+        }
+
+        return _games[game].Take(10);
+    }
+
+    public bool DeleteGame(string game, string gamePassword)
+    {
+        if (!_gamesPaswords.ContainsKey(game) 
+            || !_gamesPaswords[game].Equals(gamePassword))
+        {
+            return false;
+        }
+
+        _gamesPaswords.Remove(game);
+        _games.Remove(game);
+
+        return true;
+    }
+
+    public IEnumerable<string> ListGamesByPrefix(string gameNamePrefix)
+    {
+        var upperBound = gameNamePrefix + char.MaxValue;
+        var gamesWithPrefix = _games.Range(gameNamePrefix, true, upperBound, false);
+        return gamesWithPrefix.Take(_maxEntriesToKeep).Select(e => e.Key);
+    }
+}
---
+++ 25. Exam Preparation/Scoreboard/Scoreboard/ScoreboardEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class ScoreboardEntry : IComparable<ScoreboardEntry>
+{
+    public int Score { get; set; }
+
+    public string Username { get; set; }
+
+    public ScoreboardEntry(string username, int score)
+    {
+        Username = username;
+        Score = score;
+    }
+
+    public int CompareTo(ScoreboardEntry other)
+    {
+        if (this == other)
+        {
+            return 0;
+        }
+
+        var result = -Score.CompareTo(other.Score);
+
+        if (result == 0)
+        {
+            result = string.Compare(Username, other.Username,
+                StringComparison.InvariantCulture);
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{Username} {Score}";
+    }
+}
