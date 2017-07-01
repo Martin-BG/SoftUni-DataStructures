14c1419ecc0e90814a2ae68993456a7c466c0a84
MartinBG <MartinBG@abv.bg>
2017-07-01 15:21:17 +0300

    removed redundant files

--- 13. B-Trees and Red-Black Trees - Exercises/Hierarchy - Author's solution/Java-Skeleton/src/test/helpers/BasePerformanceTest.java
+++
@@ -1,8 +0,0 @@
-package test.helpers;
-
-import java.util.Random;
-
-public class BasePerformanceTest extends BaseTest {
-
-    protected Random random = new Random();
-}
--- 13. B-Trees and Red-Black Trees - Exercises/Hierarchy - Author's solution/Java-Skeleton/src/test/types/CorrectnessTests.java
+++
@@ -1,4 +0,0 @@
-package test.types;
-
-public interface CorrectnessTests {
-}
